e16b7ca8e734a25ac1f412a88a125450782ce953
Danielle Monroe <dmonroe08@fullsail.edu>
2016-10-12 20:51:44 -0400

    Fixes Home button in nav

--- CodeIgniter-3.1.0/application/views/includes/header.php
+++ CodeIgniter-3.1.0/application/views/includes/header.php
@@ -27,7 +27,7 @@
 											<!--Navigation-->
 										<ul class="nav nav-tabs nav-justified" role="nav">
 											<div class="row">
-											<div class="col-md-4"><div class="text-center"><li class="go" role="presentation"><a class="nav-button" href="<?php echo site_url() . '/Welcome/load';?>">Home</a></li></div></div>
+											<div class="col-md-4"><div class="text-center"><li class="go" role="presentation"><a class="nav-button" href="<?php echo site_url() . '/Welcome/index';?>">Home</a></li></div></div>
 											<div class="col-md-4"><div class="text-center"><li class="inactive" role="presentation"><a class="nav-button" href="<?php echo site_url() . '/signin/load';?>">Sign In</a></li></div></div>
 											<div class="col-md-4"><div class="text-center"><li class="inactive" role="presentation"><a class="nav-button" href="<?php echo site_url() . '/signup/load';?>">Sign Up</a></li></div></div>
 											</div>
